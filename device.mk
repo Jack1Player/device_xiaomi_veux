bb6b5f1fcc68dff7c23fa249a4fb1a60fe372fdb
orges <me@orgesified.ml>
2022-07-03 15:01:14 -0300

    veux: Remove telephony-ext from boot jars

--- device.mk
+++ device.mk
@@ -478,9 +478,6 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Update Engine
 PRODUCT_PACKAGES += \
     update_engine \
